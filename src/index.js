b09a13cc471ec94a646053c6be8c3b4731908ce5
AdminNormTherapy <111477333+AdminNormTherapy@users.noreply.github.com>
2022-09-18 11:30:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -83,10 +83,10 @@ const App = () => {
           audio: false,
           video: {
             mandatory: {
-              minWidth: 320,
-              maxWidth: 320,
-              minHeight: 240,
-              maxHeight: 240,
+              minWidth: 640,
+              maxWidth: 640,
+              minHeight: 480,
+              maxHeight: 480,
               minFrameRate: 1,
               maxFrameRate: 10
             }
