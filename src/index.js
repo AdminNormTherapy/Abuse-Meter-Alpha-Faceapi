c2e1e125d3f9b64ce0a3b1beefc176439e7c2396
yourmindsetremodel <113265377+yourmindsetremodel@users.noreply.github.com>
2022-10-26 12:14:41 -0400

    Revert "Update index.js"
    This reverts commit 4edb7e59145559358e2105876f62f4cda4d22a9b.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import {
 import "./styles.css";
 import { Amplify, API, graphqlOperation } from "aws-amplify";
 
-import awsconfig from "./aws-exports.js";
+import awsconfig from "./aws-exports";
 import { createTodo } from "./graphql/mutations";
 
 Amplify.configure(awsconfig);
@@ -74,6 +74,8 @@ const App = () => {
           setDetected(true);
           const dims = matchDimensions(canvas, video, true);
           const resizedResults = resizeResults(faces, dims);
+          let jsonString = JSON.stringify(resizedResults);
+	  localStorage.setItem('faceapi_net.json', jsonString);
           console.log(resizedResults);
 		  createNewTodo(resizedResults);
           if (true) {
