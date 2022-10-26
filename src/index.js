5f65d075efe32d0379abe9b3768ac0e24617986b
yourmindsetremodel <113265377+yourmindsetremodel@users.noreply.github.com>
2022-10-26 12:14:46 -0400

    Revert "Merge branch 'main' of https://github.com/AdminNormTherapy/Abuse-Meter-Alpha-Faceapi"
    This reverts commit 908c7de42dbede6baeeed0324d9ef9ecc903e47c, reversing
    changes made to f917af9b0f4761a409767520e0c5026f18446da6.

--- src/index.js
+++ src/index.js
@@ -74,8 +74,6 @@ const App = () => {
           setDetected(true);
           const dims = matchDimensions(canvas, video, true);
           const resizedResults = resizeResults(faces, dims);
-          let jsonString = JSON.stringify(resizedResults);
-	  localStorage.setItem('faceapi_net.json', jsonString);
           console.log(resizedResults);
 		  createNewTodo(resizedResults);
           if (true) {
